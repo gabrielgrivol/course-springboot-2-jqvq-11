b0edca4ea2a14f36115b882c742b861b5a7a9d94
gabrielgrivol <jggl@terra.com.br>
2021-06-10 09:02:15 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/glf/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.glf.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.glf.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
