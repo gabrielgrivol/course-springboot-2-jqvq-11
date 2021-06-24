8d740c9b3afdfeec92daebebfef692049baa8635
gabrielgrivol <jggl@terra.com.br>
2021-06-24 09:30:34 -0300

    User update

--- src/main/java/com/glf/course/services/UserService.java
+++ src/main/java/com/glf/course/services/UserService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.glf.course.entities.Order;
 import com.glf.course.entities.User;
 import com.glf.course.repositories.UserRepository;
 
@@ -31,7 +30,20 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
-		
+	}	
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
 	}
 
+	private void updateData(User entity, User obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setFone(obj.getFone());
+		
+		
+	}
+	
 }
